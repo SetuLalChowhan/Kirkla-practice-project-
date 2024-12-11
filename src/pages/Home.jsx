27f57e99533a0b24b75fb4439aff537ceaf667ee
setulal chowhan <setulalchowhan@gmail.com>
2024-12-11 17:49:32 +0600

    practice project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import banner from '../assets/images/Vector-2.png'
+import Banner1 from '../components/Home/Banner1'
+
+const Home = () => {
+  return (
+    <div className=" ">
+        <Banner1/>
+      
+  </div>
+  )
+}
+
+export default Home
